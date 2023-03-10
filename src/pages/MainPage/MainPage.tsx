f8717fdb5aac1e749d874ed5c3a9486cda8f9a95
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-03-10 21:49:38 +0400

    feat: add templates of Main and 404 pages

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './MainPage.module.scss';
+
+class MainPage extends React.PureComponent {
+  render() {
+    return <main className={styles.main}>Main Page</main>;
+  }
+}
+
+export default MainPage;
