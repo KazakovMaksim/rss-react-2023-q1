2ea785bc070e9beb98d9b39cab2fb438eaf687a4
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-03-10 23:00:04 +0400

    feat: use wrapper for pages

--- src/pages/RootPage/RootPage.tsx
+++ src/pages/RootPage/RootPage.tsx
@@ -1,16 +1,17 @@
 import Footer from 'components/Footer';
 import Header from 'components/Header';
+import Wrapper from 'components/Wrapper';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 class RootPage extends React.PureComponent {
   render() {
     return (
-      <>
+      <Wrapper>
         <Header />
         <Outlet />
         <Footer />
-      </>
+      </Wrapper>
     );
   }
 }
