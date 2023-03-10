0a51a54211bed03f337cb7e5d71c9b80ed9ee157
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-03-10 22:55:46 +0400

    feat: add AboutUs page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NotFoundPage from './pages/RootPage/NotFoundPage';
 import MainPage from './pages/RootPage/MainPage';
 import RootPage from './pages/RootPage';
+import AboutUsPage from './pages/RootPage/AboutUsPage';
 
 class App extends React.PureComponent {
   render() {
@@ -11,6 +12,7 @@ class App extends React.PureComponent {
         <Routes>
           <Route path="/" element={<RootPage />}>
             <Route index element={<MainPage />} />
+            <Route path="about" element={<AboutUsPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
