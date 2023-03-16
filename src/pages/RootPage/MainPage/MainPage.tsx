369deed4df489201efbb980a571a12ebbc785980
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-03-16 20:32:38 +0400

    feat: add cards to the Main page

--- src/pages/RootPage/MainPage/MainPage.tsx
+++ src/pages/RootPage/MainPage/MainPage.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import Card from '../../../components/Card';
+import cardsData from '../../../data/cards';
 
 import styles from './MainPage.module.scss';
 
 class MainPage extends React.PureComponent {
   render() {
-    return <main className={styles.main}>Main Page</main>;
+    const cards = cardsData.map((card) => (
+      <Card
+        key={card.id}
+        imgSrc={card.imgSrc}
+        price={card.price}
+        description={card.description}
+        title={card.title}
+      />
+    ));
+
+    return <main className={styles.main}>{cards}</main>;
   }
 }
 
