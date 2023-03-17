4b15c091736a3b19b636a10d942b1e917a8deea5
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-03-17 10:51:19 +0400

    feat: move base index.css into style folder, add mixins&variables, add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -14,6 +16,7 @@ export default defineConfig({
       assets: '/src/assets',
       data: '/src/data',
       constants: '/src/constants',
+      styles: '/src/styles',
     },
   },
   test: {
