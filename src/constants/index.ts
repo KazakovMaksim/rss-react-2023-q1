6dbf604f4cb9cf3e64d39860ac1c003db9215e65
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-03-22 18:55:26 +0400

    feat: add Form page

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,7 @@
 const headerLinks = [
   { name: 'Home', path: '/' },
   { name: 'About Us', path: '/about' },
+  { name: 'Form Fill', path: '/form' },
 ];
 
 export default headerLinks;
---
+++ src/pages/RootPage/FormPage/FormPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './FormPage.module.scss';
+
+class FormPage extends React.PureComponent {
+  render() {
+    return <main className={styles.form}>This is a Form Page</main>;
+  }
+}
+
+export default FormPage;
