9472426f63795b4db68bf06e9daf0bded292de19
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-03-24 11:09:03 +0400

    feat: add default profile images src links

--- src/components/Header/NavBar/NavBar.tsx
+++ src/components/Header/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { NavLink } from 'react-router-dom';
-import headerLinks from 'constants/index';
+import { headerLinks } from 'constants/index';
 
 import styles from './NavBar.module.scss';
 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,11 @@
-const headerLinks = [
+export const headerLinks = [
   { name: 'Home', path: '/' },
   { name: 'About Us', path: '/about' },
   { name: 'Form Fill', path: '/form' },
 ];
 
-export default headerLinks;
+export const userDefaultImg = {
+  male: 'https://www.shutterstock.com/image-vector/man-avatar-profile-male-face-600w-471975115.jpg',
+  female:
+    'https://www.shutterstock.com/image-vector/avatar-profile-female-face-silhouette-600w-471975109.jpg',
+};
