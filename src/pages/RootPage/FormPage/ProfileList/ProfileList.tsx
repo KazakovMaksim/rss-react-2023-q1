f4b1294aa1f84da7c1186c3499066ba97c626275
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-03-26 12:04:02 +0400

    refactor: pass entire card instead of spreading

--- src/pages/RootPage/FormPage/ProfileList/ProfileList.tsx
+++ src/pages/RootPage/FormPage/ProfileList/ProfileList.tsx
@@ -12,18 +12,8 @@ class ProfileList extends React.PureComponent<CardsItems> {
   render() {
     const { cards } = this.props;
     const list = cards.map((card: FormDataItem) => {
-      const { user, gender, phone, email, birthday, extra } = card;
-      return (
-        <ProfileCard
-          key={user}
-          user={user}
-          gender={gender}
-          phone={phone}
-          email={email}
-          birthday={birthday}
-          extra={extra}
-        />
-      );
+      const { user } = card;
+      return <ProfileCard key={user} card={card} />;
     });
     const deliveryList = cards.length ? list : <div>profiles list is still empty</div>;
 
