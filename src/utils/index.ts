8c29f68cfd4c5c9d923cf53ee04265d5de8999a5
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-03-27 00:09:00 +0400

    feat: add date validation - later than now, and 10yo

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,26 +1,51 @@
 import { validation } from 'constants/index';
 
+const checkBirthday = (date: string) => {
+  let isValid = true;
+  const now = new Date();
+  const birth = new Date(date);
+  const age = now.getFullYear() - birth.getFullYear();
+  if (birth > now || age < 10) {
+    isValid = false;
+  }
+  return isValid;
+};
+
 const validateField = (fieldName: string, fieldValue?: string | File) => {
   let errName = '';
-  if (
-    fieldName === 'user' &&
-    typeof fieldValue === 'string' &&
-    fieldValue.trim().length < validation.userMinLength
-  ) {
-    errName = `field must have min ${validation.userMinLength} letter length`;
-  }
 
-  if (
-    fieldName === 'user' &&
-    typeof fieldValue === 'string' &&
-    fieldValue.trim().length >= validation.userMinLength &&
-    !validation.userPattern.test(fieldValue)
-  ) {
-    errName = `name must start with uppercase letter`;
-  }
+  if (typeof fieldValue === 'string') {
+    if (fieldName === 'user' && fieldValue.trim().length < validation.userMinLength) {
+      errName = `field must have min ${validation.userMinLength} letter length`;
+    }
 
-  if (fieldName === 'file' && (fieldValue as File).type !== validation.fileType) {
-    errName = 'please, use only image files';
+    if (
+      fieldName === 'user' &&
+      fieldValue.trim().length >= validation.userMinLength &&
+      !validation.userPattern.test(fieldValue)
+    ) {
+      errName = `name must start with uppercase letter`;
+    }
+
+    if (fieldName === 'phone' && fieldValue.trim().length < validation.phoneMinLength) {
+      errName = `field must have min ${validation.phoneMinLength} digits length`;
+    }
+
+    if (
+      fieldName === 'phone' &&
+      fieldValue.trim().length >= validation.phoneMinLength &&
+      !validation.phonePattern.test(fieldValue)
+    ) {
+      errName = `phone must have only digits`;
+    }
+
+    if (fieldName === 'email' && !validation.emailPattern.test(fieldValue.trim())) {
+      errName = `entered email is not correct, check it`;
+    }
+
+    if (fieldName === 'birth' && !checkBirthday(fieldValue)) {
+      errName = `you must be over 10 years old and birthday can't be later than now`;
+    }
   }
 
   if (fieldValue === '') {
