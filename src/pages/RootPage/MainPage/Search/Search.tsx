73c587e1c3d26f3302d9f8d6a16c54c1966e06c1
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-03-30 15:53:58 +0300

    refactor: retain functionality and rewrite Search component

--- src/pages/RootPage/MainPage/Search/Search.tsx
+++ src/pages/RootPage/MainPage/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Button from 'components/Button';
 import Input from 'components/Input';
@@ -9,46 +9,39 @@ type InputState = {
   readonly value: string;
 };
 
-class Search extends React.Component<object, InputState> {
-  constructor(props: object) {
-    super(props);
+const Search = () => {
+  const [value, setValue] = React.useState<string>('');
+  let inputValue = React.useRef<string>(value);
 
-    this.state = {
-      value: '',
-    };
-  }
-
-  componentDidMount() {
-    const inputValueLS = localStorage.getItem('inputValueLS');
-    if (inputValueLS) {
-      this.setState({ value: inputValueLS });
-    }
-  }
-
-  componentWillUnmount() {
-    const { value } = this.state;
-    localStorage.setItem('inputValueLS', value);
-  }
-
-  handleInput = (newValue: string) => {
-    this.setState({ value: newValue });
+  const handleInput = (newValue: string) => {
+    setValue(newValue);
   };
 
-  render() {
-    const { value } = this.state;
+  useEffect(() => {
+    const inputValueLS = localStorage.getItem('inputValueLS');
 
-    return (
-      <div className={styles.search}>
-        <Input
-          value={value}
-          className={styles.search_input}
-          placeholder="Search property"
-          onChange={this.handleInput}
-        />
-        <Button className={styles.search_btn}>Find</Button>
-      </div>
-    );
-  }
-}
+    if (inputValueLS) {
+      setValue(inputValueLS);
+    }
+
+    return () => {
+      localStorage.setItem('inputValueLS', inputValue.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    inputValue.current = value;
+  }, [value]);
+
+  return <div className={styles.search}>
+      <Input
+        value={value}
+        className={styles.search_input}
+        placeholder="Search property"
+        onChange={handleInput}
+      />
+      <Button className={styles.search_btn}>Find</Button>
+    </div>;
+};
 
 export default Search;
