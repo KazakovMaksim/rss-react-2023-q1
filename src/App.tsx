1cdc281af1f1b3bae6d7a15df4142134f41640cd
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-04-05 16:17:38 +0400

    feat: rewrite NotFoundPage, RootPage and App to func components

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,19 @@ import RootPage from './pages/RootPage';
 import AboutUsPage from './pages/RootPage/AboutUsPage';
 import FormPage from './pages/RootPage/FormPage';
 
-class App extends React.PureComponent {
-  render() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<RootPage />}>
-            <Route index element={<MainPage />} />
-            <Route path="about" element={<AboutUsPage />} />
-            <Route path="form" element={<FormPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootPage />}>
+          <Route index element={<MainPage />} />
+          <Route path="about" element={<AboutUsPage />} />
+          <Route path="form" element={<FormPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
