aad7fd63d4e89008f2414434f7c1c2166598962c
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-04-17 10:07:03 +0400

    refactor: change errors handling

--- src/pages/RootPage/MainPage/MainPage.tsx
+++ src/pages/RootPage/MainPage/MainPage.tsx
@@ -3,6 +3,7 @@ import Card from 'components/Card';
 import Loading from 'components/Loading';
 import { useGetProductsQuery } from 'store/api/api';
 import usedTypedSelector from 'hooks/useTypedSelector';
+import { ErrorItem } from 'types';
 import Modal from './Modal';
 import Search from './Search';
 import styles from './MainPage.module.scss';
@@ -15,7 +16,6 @@ export enum InfoMessages {
 const MainPage = () => {
   const [isModalOpened, setIsModalOpened] = useState(false);
   const [cardIdForModal, setCardIdForModal] = useState('');
-  const [apiReqErrorName, setApiReqErrorName] = useState('');
   const { searchValue } = usedTypedSelector((state) => state.products);
   const { isLoading, error, data: productCards } = useGetProductsQuery(searchValue, {});
 
@@ -24,7 +24,9 @@ const MainPage = () => {
     setIsModalOpened(!isModalOpened);
   };
 
-  const infoBlock = apiReqErrorName ? InfoMessages.GettingCardErr : InfoMessages.Empty;
+  const infoBlock = error
+    ? `${InfoMessages.GettingCardErr} (${error && (error as ErrorItem).status})`
+    : InfoMessages.Empty;
 
   return (
     <main className={styles.main}>
@@ -39,7 +41,7 @@ const MainPage = () => {
           productCards.map((product) => (
             <Card key={product.id} product={product} onClick={(id) => cardClickHandle(id)} />
           ))}
-        {productCards && productCards.length === 0 && (
+        {(!productCards || productCards?.length === 0) && (
           <div className={styles.main_info}>{infoBlock}</div>
         )}
       </div>
